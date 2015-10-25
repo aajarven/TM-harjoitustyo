b05399ff3ecc86bc03aa1a3a2b23099e7f4d738c
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2015-10-25 19:03:53 +0200

    mahdollisuus antaa joko x-havainnot tai valmis matriisi

--- python/pns.py
+++ python/pns.py
@@ -13,11 +13,13 @@ xHavainto = lueMatriisi(sys.argv[2])
 Y = lueMatriisi(sys.argv[3])
 W = lueMatriisi(sys.argv[4])
 
-X = np.ones((len(xHavainto), aste+1))
-for i in range(0,len(xHavainto)):
-    for j in range(1, aste+1):
-        X[i][j] = xHavainto[i]**j
-
+if len(np.transpose(xHavainto)) == 1 : # jos annetussa X-matriisissa on vain yksi sarake, oletetaan n. asteen käyrä y=f(x) ja luodaan X-matriisi sen mukaan
+    X = np.ones((len(xHavainto), aste+1))
+    for i in range(0,len(xHavainto)):
+        for j in range(1, aste+1):
+            X[i][j] = xHavainto[i]**j
+else : # muuten käytetään annettua sellaisenaan
+    X = xHavainto
 beta = np.dot( np.dot( np.dot( np.linalg.inv( np.dot( np.dot( np.transpose(X), W), X)), np.transpose(X)), W), Y)
 print "kerroinmatriisi beta:"
 print beta
