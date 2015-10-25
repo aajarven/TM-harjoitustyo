affeeb5f7ed95625946950a8291f6a7cf6072a05
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2015-10-25 03:26:44 +0300

    lukee annetuista tiedostoista, toimii vain 1. asteen käyrälle

--- python/pns.py
+++ python/pns.py
@@ -1,26 +1,41 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-havaintoX = np.matrix('1; 2; 3; 4; 5')
-Y = np.matrix('1.2; 2.5; 2.8; 4.5; 5.1')
+if len(sys.argv) !=4 :
+    print ("Anna komentoriviargumentteina tiedostot, joissa käytettävät matriisit X, Y ja W ovat")
+    sys.exit(1)
 
-X = np.zeros(shape=(5,2))
+f = open ( sys.argv[1] , 'r')
+X = [ map(float,line.split(',')) for line in f ]
+X = np.array(X)
 
-for i in range(0, 5):
-	for j in range(0, 2):
-		X[i][j] = havaintoX[i][0]**j
+f = open ( sys.argv[2] , 'r')
+Y = [ map(float,line.split(',')) for line in f ]
+Y = np.array(Y)
 
-beta = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X), X)),np.transpose(X)),Y)
-print beta
-print beta[0][0]
+f = open ( sys.argv[3] , 'r')
+W = [ map(float,line.split(',')) for line in f ]
+W = np.array(W)
+
+#beta = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X), X)),np.transpose(X)),Y)
+beta = np.dot(np.dot(np.dot(np.linalg.inv(np.dot(np.dot(np.transpose(X), W), X)), np.transpose(X)), W), Y)
+#print beta
+#print beta[0][0]
 
 b = beta.item(0,0)
 k = beta.item(1,0)
 
-plt.scatter(np.array(havaintoX), np.array(Y))
-plt.plot([0, 5], [b, b+5*k] )
+xmin = np.amin(X[:,1])
+xmax = np.amax(X[:,1])
+ymin = np.amin(Y)
+ymax = np.amax(Y)
+pad = 0.2
+
+plt.scatter(X[:,1], Y)
+plt.plot([xmin-pad, xmax+pad], [b+(ymin-pad)*k, b+(ymax+pad)*k] )
 axes = plt.gca()
-axes.set_xlim([0,5])
-axes.set_ylim([0,5])
+axes.set_xlim([np.amin(X[:,1])-pad, np.amax(X[:,1])+pad])
+axes.set_ylim([np.amin(Y)-pad, np.amax(Y)+pad])
 plt.show()
